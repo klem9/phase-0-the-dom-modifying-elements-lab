383f9f99bc27e4085035b9e2d6731f451ce3448e
Klemense <klemenseloh@gmail.com>
2022-02-07 13:14:14 -0500

    commit tree 43680714969716c55285574097815e920ba3d7fc

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// Write your code here!
-const main = document.getElementById('main')
+main.remove(main);
 
-main.remove()
-
-const newHeader = document.getElementById('victory')
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+document.body.append(newHeader);
+newHeader.textContent = 'YOUR-NAME is the champion';
